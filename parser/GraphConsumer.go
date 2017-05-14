18ed06b3c9ad3493c0dbf558ff756633630d4ec4
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-05-14 14:09:13 -0400

    rename

--- parser/GraphConsumer.go
+++ parser/GraphConsumer.go
@@ -58,6 +58,84 @@ func (t *TestGraphConsumer) Report() {
 	// if (err != nil){panic(err)}
 }
 
+func AbstractValue(nt string , ft string) (string){
+	//
+	if ft =="accs"{  		return "Scalar"
+	} else if ft =="algn"{		return "Scalar"
+	} else if ft =="args"{          return "Contained"     // "function_decl"->"parm_decl"		
+	} else if ft =="argt"{		return "TypeReference" // param_decl -> some type
+	} else if ft =="base"{		return "TypeReference" // record type -> record type
+	} else if ft =="bases"{         return "Scalar"		
+	} else if ft =="bfld"{		return "TypeReference" // record type -> record type
+	} else if ft =="binf"{		return "TypeReference" // record type/union
+	} else if ft =="body"{		return "Contained"
+	} else if ft =="bpos"{		return "ContainedScalar"
+	} else if ft =="chain"{		return "Skip"          // skip for now
+	} else if ft =="chan"{		return "Skip"          // todo
+	} else if ft =="clas"{		return "TypeReference"   // method -> record type
+	} else if ft =="cnst"{		return "ContainedScalar" // const decl -> integer constant
+	} else if ft =="cond"{          return "Contained"      // stmts and exprs
+	} else if ft =="crnt"{		return "TypeReference"  // templates overload/ function decl/template_decl
+	} else if ft =="csts"{		return "ContainedList"  // list of constants
+	} else if ft =="ctor"{		return "Scalar"
+	} else if ft =="dcls"{		return "ContainedList" // list of decls in a namespace
+	} else if ft =="decl"{		return "Contained"     // target_expr/var_decl/aggr_init_expr
+	} else if ft =="domn"{		return "TypeReference"
+	} else if ft =="else"{		return "Contained"     // if/bind
+	} else if ft =="elts"{		return "TypeReference" // array element types
+	} else if ft =="expr"{		return "Contained"
+	} else if ft =="externbody"{    return "Error"	       // errr
+	} else if ft =="flds"{		return "Contained"     // types contain fields and others
+	} else if ft =="fn"{		return "Contained"     // types contain fields and others
+	} else if ft =="fncs"{		return "Contained"     // types contain fields and others
+	} else if ft =="idx"{		return "Contained"     // types contain fields and others
+	} else if ft =="init"{		return "Contained"
+	} else if ft =="inst"{		return "ContainedList" // template / list
+	} else if ft =="int"{		return "Scalar"
+	} else if ft =="labl"{		return "Contained"     // goto-label
+	} else if ft =="lang"{		return "Scalar"
+	} else if ft =="line"{		return "Scalar"
+	} else if ft =="link"{		return "Scalar"
+	} else if ft =="lngt"{		return "Scalar"
+	} else if ft =="low"{		return "Contained"     // integer const
+	} else if ft =="max"{		return "Contained"
+	} else if ft =="min"{ 		return "Contained"
+	} else if ft =="mngl"{		return "Contained"
+	} else if ft =="name"{		return "Contained"
+	} else if ft =="nodetype"{	return "Type"
+	} else if ft =="note"{          return "Scalar"
+	} else if ft =="op_0"{		return "Contained"
+	} else if ft =="op_1"{		return "Contained"
+	} else if ft =="op_2"{		return "Contained"
+	} else if ft =="orig"{		return "Reference"
+	} else if ft =="prec"{		return "Scalar"
+	} else if ft =="prms"{		return "ContainedList" // -> tree_list
+	} else if ft =="ptd"{		return "Reference"
+	} else if ft =="purp"{		return "Contained"
+	} else if ft =="qual"{		return "Reference"
+	} else if ft =="refd"{		return "Reference"
+	} else if ft =="retn"{		return "Contained"
+	} else if ft =="rslt"{		return "Contained"
+	} else if ft =="scpe"{		return "Container"
+	} else if ft =="sign"{		return "Scalar"
+	} else if ft =="size"{		return "Reference"
+	} else if ft =="spcs"{		return "ContainedList"
+	} else if ft =="spec"{		return "Scalar"
+	} else if ft =="srcp"{		return "Contained"
+	} else if ft =="strg"{		return "Contained"
+	} else if ft =="tag"{		return "Scalar"
+	} else if ft =="then"{		return "Contained"
+	} else if ft =="type"{		return "Reference"
+	} else if ft =="unql"{		return "Reference"
+	} else if ft =="used"{		return "Scalar"
+	} else if ft =="val"{		return "Contained"
+	} else if ft =="valu"{		return "Contained"
+	} else if ft =="vars"{		return "Contained"
+	} else if ft =="vfld"{		return "Contained"
+	} else {	                return "Contained" }
+	return ""	
+}
+
 func (t *TestGraphConsumer) NodeImp(n *TestGraphNode) {
 	//t.Lock.Lock()
 	//defer t.Lock.Unlock()
@@ -71,7 +149,14 @@ func (t *TestGraphConsumer) NodeImp(n *TestGraphNode) {
 		vals := strings.Join(v, "|")
 		//t.Transaction.AddQuad(quad.Make(nid, k, vals,""))//, n.Filename
 		//"71014" "type:" "43207" "" .
-		t.Outfile.WriteString(fmt.Sprintf("\"%s\" \"%s\" \"%s\" \"\".\n",nid,k,Escape(vals)))
+		v2 := Escape(vals)
+		t.Outfile.WriteString(fmt.Sprintf("\"%s\" \"%s\" \"%s\" \"\".\n",nid,k,v2))
+
+		// now we abstract the fieldname based on the nodetype and fieldname
+		k2 := AbstractValue(n.NodeType,k)
+		if k2 != "" {
+			t.Outfile.WriteString(fmt.Sprintf("\"%s\" \"%s\" \"%s\" \"\".\n",nid,k2,v2))
+		}
 	}	
 }
 

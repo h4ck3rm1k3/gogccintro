b1a13678a27c61914d9c5576ab4ee0d4be260764
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-05-16 06:33:48 -0400

    update

--- docs/gcc/CallGraph/gendot.sh
+++ docs/gcc/CallGraph/gendot.sh
@@ -1,3 +1,3 @@
-echo "digraph {" > test.dot
-cat test.json | jq -f dot.jq -s -r >>  test.dot
-echo "}" >> test.dot
+echo "digraph {" > static/callgraph/treedump/test.dot
+cat static/callgraph/treedump/test.json | jq -f dot.jq -s -r >>  static/callgraph/treedump/test.dot
+echo "}" >> static/callgraph/treedump/test.dot
